aad8c0738ca425706bb6fa1fea1bcc5fc1d63724
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-15 19:47:16 +0800

    ADD: script for tracking borrows. (BETA)
    on BETA, as it needs more improvements and needs to align with what the attribute for the book would be, which would be worked on by @LVen

---
+++ Question1e.sql
@@ -0,0 +1,16 @@
+CREATE TABLE borrow(
+    borrow_id INT AUTO_INCREMENT,
+    item_id VARCHAR(100) NOT NULL,
+	borrower_id VARCHAR(15) NOT NULL,
+	start_date DATE NOT NULL,
+	end_date DATE, 
+	PRIMARY KEY(borrow_id),
+	FOREIGN KEY (item_id) 
+		REFERENCES book(book_id),
+	FOREIGN KEY(borrower_id)
+		REFERENCES student(student_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(borrower_id)
+		REFERENCES instructor(instructor_id)
+        ON DELETE CASCADE ON UPDATE CASCADE
+);
