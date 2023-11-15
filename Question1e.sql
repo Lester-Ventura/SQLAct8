60289b45cc55ec88013e173ed28c9e14bcfb6262
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-15 21:09:11 +0800

    UPDATE: fixed syntax errors in borrow table

--- Question1e.sql
+++ Question1e.sql
@@ -1,16 +1,16 @@
-CREATE TABLE borrow(
+CREATE TABLE borrow (
     borrow_id INT AUTO_INCREMENT,
     item_id VARCHAR(255) NOT NULL,
-	borrower_id VARCHAR(15) NOT NULL,
-	start_date DATE NOT NULL,
-	end_date DATE, 
+    borrower_id VARCHAR(15) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE, 
 	PRIMARY KEY(borrow_id),
 	FOREIGN KEY (item_id) 
-		REFERENCES book(book_id),
-		ON DELETE CASCADE ON UPDATE CASCADE
+		REFERENCES book(book_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY (item_id) 
-		REFERENCES equipment(equipment_id),
-		ON DELETE CASCADE ON UPDATE CASCADE
+		REFERENCES equipment(equipment_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY(borrower_id)
 		REFERENCES student(student_id)
 		ON DELETE CASCADE ON UPDATE CASCADE,
