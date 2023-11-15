97bcdf2c3a9448cca3e5d85641dec78a3a37c5de
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-15 21:05:16 +0800

    UPDATE: fixed error syntaxes

--- Question1f.sql
+++ Question1f.sql
@@ -1,28 +1,20 @@
-CREATE TABLE room (
-    room_id INT NOT NULL,
-    max_capacity INT NOT NULL,
-    room_type ENUM("Multimedia", "Discussion") NOT NULL,
-    PRIMARY KEY(room_id)
+CREATE TABLE borrow (
+    borrow_id INT AUTO_INCREMENT,
+    item_id VARCHAR(255) NOT NULL,
+    borrower_id VARCHAR(15) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE, 
+	PRIMARY KEY(borrow_id),
+	FOREIGN KEY (item_id) 
+		REFERENCES book(book_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY (item_id) 
+		REFERENCES equipment(equipment_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(borrower_id)
+		REFERENCES student(student_id)
+		ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(borrower_id)
+		REFERENCES instructor(instructor_id)
+        	ON DELETE CASCADE ON UPDATE CASCADE
 );
-
-CREATE TABLE room_usage (
-	usage_id INT AUTO_INCREMENT,
-	room_id INT NOT NULL,
-	user_id VARCHAR(15) NOT NULL,
-	start_timestamp DATETIME NOT NULL,
-	end_timestamp DATETIME,
-    PRIMARY KEY(usage_id),
-	FOREIGN KEY(room_id)
-			REFERENCES room(room_id)
-			ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(user_id)
-			REFERENCES student(student_id)
-			ON DELETE CASCADE ON UPDATE CASCADE,
-	FOREIGN KEY(user_id)
-			REFERENCES instructor(instructor_id)
-			ON DELETE CASCADE ON UPDATE CASCADE
-);
-
-
-
-
