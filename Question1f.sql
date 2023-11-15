3a9a00edfb53783553e05913bda6003ca224f323
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-15 19:42:57 +0800

    ADD: script for library room and usage tracker

---
+++ Question1f.sql
@@ -0,0 +1,28 @@
+CREATE TABLE room (
+    room_id INT NOT NULL,
+    max_capacity INT NOT NULL,
+    room_type ENUM("Multimedia", "Discussion") NOT NULL,
+    PRIMARY KEY(room_id)
+);
+
+CREATE TABLE room_usage (
+	usage_id INT AUTO_INCREMENT,
+	room_id INT NOT NULL,
+	user_id VARCHAR(15) NOT NULL,
+	start_timestamp DATETIME NOT NULL,
+	end_timestamp DATETIME,
+    PRIMARY KEY(usage_id),
+	FOREIGN KEY(room_id)
+			REFERENCES room(room_id)
+			ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(user_id)
+			REFERENCES student(student_id)
+			ON DELETE CASCADE ON UPDATE CASCADE,
+	FOREIGN KEY(user_id)
+			REFERENCES instructor(instructor_id)
+			ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
