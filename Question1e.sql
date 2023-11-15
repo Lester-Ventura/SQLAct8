f5c35114169369e98dbdb4be4aca21866e30591c
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-15 20:20:47 +0800

    UPDATE: borrow table is now linked to both equipment and book

--- Question1e.sql
+++ Question1e.sql
@@ -7,10 +7,14 @@ CREATE TABLE borrow(
 	PRIMARY KEY(borrow_id),
 	FOREIGN KEY (item_id) 
 		REFERENCES book(book_id),
+		ON DELETE CASCADE ON UPDATE CASCADE
+	FOREIGN KEY (item_id) 
+		REFERENCES equipment(equipment_id),
+		ON DELETE CASCADE ON UPDATE CASCADE
 	FOREIGN KEY(borrower_id)
 		REFERENCES student(student_id)
 		ON DELETE CASCADE ON UPDATE CASCADE,
 	FOREIGN KEY(borrower_id)
 		REFERENCES instructor(instructor_id)
-        ON DELETE CASCADE ON UPDATE CASCADE
+        	ON DELETE CASCADE ON UPDATE CASCADE
 );
