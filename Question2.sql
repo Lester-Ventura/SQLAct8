3325660478ababe33c0574680c40b70d97388a53
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-15 22:22:38 +0800

    ADD: script for populating the student records (collaborative file)

---
+++ Question2.sql
@@ -0,0 +1,16 @@
+-- from @etdvprg
+INSERT INTO student (student_id, last_name, first_name, permanent_address, current_address, contact_number, contact_email, birthdate, program, enrollment_year) VALUES (020220319000001, "Cruzat", "Slaybells Batumbakal", "Consunji St., San Jose, City of San Fernando, Pampanga", "Padre Rada St., Tondo, Manila", 09060938294, "fanimingyu@gmail.com", "2003-12-19", "BSCS", 2022);
+INSERT INTO student (student_id, last_name, first_name, middle_initial, permanent_address, current_address, contact_number, contact_email, birthdate, program, enrollment_year) VALUES (020221114030002, "Kim", "Nayeongyu", "C", "Balantang Road, Jaro, Iloilo City, Iloilo", "Filinvest Ave., Alabang, Muntinlupa", 09009533694, "kimkimmy@jype.com", "2004-02-16", "BSCS", 2022);
+INSERT INTO student (student_id, last_name, first_name, middle_initial, permanent_address, current_address, contact_number, contact_email, birthdate, program, enrollment_year) VALUES (020220410110003, "Dough", "Jongin", "K", "Cadena de Amor St., Pembo, Taguig", "J. Luna, Matandang Balara, Quezon City", 09089923673, "letmein@exist.com", "2005-01-03", "BSCS", 2022);
+
+
+-- from @Vladifish
+
+
+-- from @Lester-Ventura
+
+
+-- from @miggy-v
+
+
+-- from @casscodes220
