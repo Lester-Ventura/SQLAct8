433060ea1db12a11fb186fa180f0a5496e3ae5a7
Miguel Angelo Verceles <137052582+miggy-v@users.noreply.github.com>
2023-11-17 00:48:36 +0800

    Update Question2.sql

--- Question2.sql
+++ Question2.sql
@@ -11,6 +11,6 @@ INSERT INTO student (student_id, last_name, first_name, middle_initial, permanen
 
 
 -- from @miggy-v
-INSERT INTO student (student_id, last_name, first_name, middle_initial, permanent_address, current_address, contact_number, contact_email, birthdate, program, enrollment_year) VALUES (020220410010004, "Dela Cruz", "Juan", "A", "Gomez St., San Juan, Taytay, Rizal", "Arellano Ave., Beverly Hills, Antipolo City",  09162344567, "juanpogi123@yahoo.com", "2003-12-19", "BSCS", 2022);
-INSERT INTO student (student_id, last_name, first_name, middle_initial, permanent_address, current_address, contact_number, contact_email, birthdate, program, enrollment_year) VALUES (020221601110006, "Panganiban", "Axel Ronald", "K", "Vista Verde Ave., Santo Domingo, Cainta, Rizal", "Mileguas St., Bagong Ilog, Pasig City",  09163526325, "axelicious10@hotmail.com", "2002-10-26", "BSCS", 2022);
+INSERT INTO student (student_id, last_name, first_name, middle_initial, permanent_address, contact_number, contact_email, birthdate, program, enrollment_year) VALUES (020220410010004, "Dela Cruz", "Juan", "A", "Gomez St., San Juan, Taytay, Rizal",  09162344567, "ja.delacruz.student@yuste.edu", "2003-12-19", "BSCS", 2022);
+INSERT INTO student (student_id, last_name, first_name, middle_initial, permanent_address, contact_number, contact_email, birthdate, program, enrollment_year) VALUES (020221601110006, "Panganiban", "Axel Ronald", "K", "Vista Verde Ave., Santo Domingo, Cainta, Rizal", 09163526325, "ark.panganiban.student@yuste.edu", "2002-10-26", "BSCS", 2022);
 -- from @casscodes220
