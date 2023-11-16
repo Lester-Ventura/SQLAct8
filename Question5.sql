bc115cfc396d55dff37c68374de3b65626c35f96
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-17 00:49:20 +0800

    ADD: script for populating the library (unfinished)

---
+++ Question5.sql
@@ -0,0 +1,18 @@
+-- from @etdvprg
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“GreatF19252007”, “The Great Gatsby”, “Fitzgerald”, “Francis Scott”, “1925-04-10”, 2007, 12);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“WhiteL19062010”, “White Fang”, “London”, “Jack”, “1906-05-01”, 2010, 3);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“BFGD19822009”, “The BFG”, “Dahl”, “Roald”, “1982-01-14”, 2009, 5);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“CallA20072021”, “Call Me by Your Name”, “Aciman”, “André”, “2007-01-23”, 2021, 15);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“JaneB18472006”, “Jane Eyre”, “Brontë”, “Charlotte”, “1847-10-19”, 2006, 4);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“TwoO19972021”, “The Two Koreas”, “Oberdorfer”, “Don”, “1997-10-5”, 2021, 12);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“QuantumK20232023”, “Quantum Supremacy”, “Kaku”, “Michio”, “2023-05-02”, 2023, 1);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“SymplecticL19872021”, “Symplectic Geometry and Analytical Mathematics”, “Libermann”, “Paulette”, “1987-03-31”, 2021, 1);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, bookacq_year, book_qty) VALUES (“KokoroN19131999”, “Kokoro”, “Natsume”, “Soseki”, “1914-08-11”, 1999, 2);
