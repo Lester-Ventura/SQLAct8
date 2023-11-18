a24dd880776e882be1b62134b081096180e71070
cass <149061009+casscodes22o@users.noreply.github.com>
2023-11-18 12:14:08 +0800

    UPDATE: ambag q 18/11/2023

--- Question5.sql
+++ Question5.sql
@@ -16,3 +16,26 @@ INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_n
 INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("SymplecticL19872021", "Symplectic Geometry and Analytical Mathematics", "Libermann", "Paulette", "1987-03-31", 2021, 1);
 
 INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("KokoroN19131999", "Kokoro", "Natsume", "Soseki", "1914-08-11", 1999, 2);
+
+-- from @casscode22o
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('MeinH19252020', 'Mein Kampf', 'Hitler', 'Adolf', '1925-07-18', 2020, 3);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('DasM18672020', 'Das Kapital', 'Marx', 'Karl', '1867-09-14', 2020, 6);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('TwilightM20052007', 'Twilight', 'Meyer', 'Stephanie', '2005-09-27', 2007, 25);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('SteelA20042020', 'Steel Ball Run', 'Araki', 'Hirohiko', '2004-01-19', 2020, 5);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('VentoA19952018', 'Vento Aureo', 'Araki', 'Hirohiko', '1995-12-11', 2018, 5);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('GameM19962012', 'A Game of Thrones', 'George R.R.', 'Martin', '1996-08-1', 2012, 33);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('ClashM19982013', 'A Clash of Kings', 'George R.R.', 'Martin', '1998-11-16', 2013, 28);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('StormM20002013', 'A Storm of Swords', 'George R.R.', 'Martin', '2000-08-08', 2013, 23);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('FeastM20052014', 'A Feast for Crows', 'George R.R.', 'Martin', '2005-10-17', 2014, 29);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('DanceM20112014', 'A Dance with Dragons', 'George R.R.', 'Martin', '2011-07-12', 2014, 35);
+
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('NoD19732020', 'No Longer Human', 'Dazai', 'Osamu', '1973-01-17', 2020, 5);
