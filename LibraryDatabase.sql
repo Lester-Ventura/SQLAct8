7691cbf365c2213e933824001d304a2bf7f9eb80
LVen <132431646+Lester-Ventura@users.noreply.github.com>
2023-11-18 13:01:06 +0800

    Numbah 5 is doneeee

--- LibraryDatabase.sql
+++ LibraryDatabase.sql
@@ -124,6 +124,25 @@ INSERT INTO instructor (instructor_id, last_name, first_name, middle_initial, pe
 /*4*/
 SELECT * FROM instructor i WHERE i.department = "CICS";
 /*5*/
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("GreatF19252007", "The Great Gatsby", "Fitzgerald", "Francis Scott", "1925-04-10", 2007, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("WhiteL19062010", "White Fang", "London", "Jack", "1906-05-01", 2010, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("BFGD19822009", "The BFG", "Dahl", "Roald", "1982-01-14", 2009, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("CallA20072021", "Call Me by Your Name", "Aciman", "André", "2007-01-23", 2021, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("JaneB18472006", "Jane Eyre", "Brontë", "Charlotte", "1847-10-19", 2006, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("TwoO19972021", "The Two Koreas", "Oberdorfer", "Don", "1997-10-5", 2021, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("QuantumK20232023", "Quantum Supremacy", "Kaku", "Michio", "2023-05-02", 2023, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("SymplecticL19872021", "Symplectic Geometry and Analytical Mathematics", "Libermann", "Paulette", "1987-03-31", 2021, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ("KokoroN19131999", "Kokoro", "Natsume", "Soseki", "1914-08-11", 1999, 5)
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('MeinH19252020', 'Mein Kampf', 'Hitler', 'Adolf', '1925-07-18', 2020, 3);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('DasM18672020', 'Das Kapital', 'Marx', 'Karl', '1867-09-14', 2020, 6);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('TwilightM20052007', 'Twilight', 'Meyer', 'Stephanie', '2005-09-27', 2007, 25);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('SteelA20042020', 'Steel Ball Run', 'Araki', 'Hirohiko', '2004-01-19', 2020, 5);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('VentoA19952018', 'Vento Aureo', 'Araki', 'Hirohiko', '1995-12-11', 2018, 5);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('GameM19962012', 'A Game of Thrones', 'George R.R.', 'Martin', '1996-08-1', 2012, 33);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('ClashM19982013', 'A Clash of Kings', 'George R.R.', 'Martin', '1998-11-16', 2013, 28);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('StormM20002013', 'A Storm of Swords', 'George R.R.', 'Martin', '2000-08-08', 2013, 23);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('FeastM20052014', 'A Feast for Crows', 'George R.R.', 'Martin', '2005-10-17', 2014, 29);
+INSERT INTO book(book_id, book_title, book_author_last_name, book_author_first_name, book_pubdate, book_acqyear, book_qty) VALUES ('DanceM20112014', 'A Dance with Dragons', 'George R.R.', 'Martin', '2011-07-12', 2014, 35);
 /*6*/
 INSERT INTO equipment (equipment_id, equipment_type, acquisition_year, total_quantity,model_type) VALUES ("VACM-00100", "Vacuum", 1400, 40, "Hoover")
 INSERT INTO equipment (equipment_id, equipment_type, acquisition_year, total_quantity,model_type) VALUES ("VACM-02077", "Vacuum", 2077, 2, "Roomba")
