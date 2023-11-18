0cc81fa86404da10f36f3762105c2fa78c55686e
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-18 14:39:47 +0800

    ADD: Question10.sql, equipment-borrowing

---
+++ Question10.sql
@@ -0,0 +1,5 @@
+INSERT INTO borrow (item_id, borrower_id, start_date, end_date) VALUES ('MRSL-00007', '020221114030002', "2023-02-10", "2023-03-20");
+INSERT INTO borrow (item_id, borrower_id, start_date, end_date) VALUES ('OVEN-42068', '120150401036668', "2023-01-20", "2023-03-20");
+INSERT INTO borrow (item_id, borrower_id, start_date, end_date) VALUES ('EPIC-12027', '120100309260010', "2023-02-16", "2023-07-09");
+INSERT INTO borrow (item_id, borrower_id, start_date, end_date) VALUES ('MOUS-82887', '020220410110003', "2023-05-10", "2023-06-22");
+INSERT INTO borrow (item_id, borrower_id, start_date, end_date) VALUES ('VACM-02077', '020221601110006', "2022-07-10", "2022-07-22");
