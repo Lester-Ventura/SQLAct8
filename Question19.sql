390e2559f4dd07158987e2f37da722facd2ebe37
Tabuzo, Enzo <74560055+etdvprg@users.noreply.github.com>
2023-11-20 18:26:26 +0800

    FIX: Question19.sql, now working.

--- Question19.sql
+++ Question19.sql
@@ -1,15 +1,16 @@
 --@etdvprg
 SELECT
-	b.borrow_id,
+    b.borrow_id,
     b.item_id,
     b.start_date,
     b.end_date,
+    b.borrower_id,
     CASE
-		WHEN s.student_id IS NOT NULL THEN 'Student'
+	WHEN s.student_id IS NOT NULL THEN 'Student'
         WHEN i.instructor_id IS NOT NULL THEN 'Instructor'
 	END AS "Borrower Type",
     CASE
-		WHEN s.student_id IS NOT NULL THEN CONCAT(s.first_name, ' ', s.last_name)
+	WHEN s.student_id IS NOT NULL THEN CONCAT(s.first_name, ' ', s.last_name)
         WHEN i.instructor_id IS NOT NULL THEN CONCAT(i.first_name, ' ', i.last_name)
 	END AS "Borrower Name",
     e.equipment_type,
@@ -19,7 +20,7 @@ SELECT
 	LEFT JOIN
         equipment e ON b.item_id = e.equipment_id
 	LEFT JOIN
-        student s ON b.borrower_id = s.student_id
+        student s ON b.borrower_id = CONCAT(0, s.student_id)
 	LEFT JOIN
         instructor i ON b.borrower_id = i.instructor_id
 	WHERE
