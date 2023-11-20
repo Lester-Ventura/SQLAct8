786c6574a40db253acadfde19efe5308de4b8a3f
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-11-20 23:12:21 +0800

    Question 16, tested but could be checked again

--- Question16.sql
+++ Question16.sql
@@ -2,30 +2,26 @@
 -- room/book/equipment, date borrowed, and date to return
 -- (in the case of a room, the ending date excluding time).
 SELECT
-    lib.BORROWER_ID AS PROFESSOR_ID,
-    lib.ITEM_ID,
-    lib.START_DATE DATE_BORROWED,
-    lib.END_DATE DATE_TO_RETURN,
+    lib.ID AS "Professor ID",
+    lib.ITEM_ID as "Item ID",
+    lib.START_DATE as "Date Borrowed",
+    lib.END_DATE as "Return by Date",
+   	lib.item_type as "Item Type"
 FROM
     (
-        SELECT
-            instructor_id id,
-            item.id item_id,
-            bor.start_date start_date,
-            bor.end_date end_date
+        SELECT DISTINCT 
+            bor.borrower_id id,
+            bor.item_id,
+            CONVERT(bor.start_date, DATE) as start_date,
+            CONVERT(bor.end_date, DATE) as end_date,
+            CASE 
+    			WHEN bor.item_id = bk.book_id then "Book"
+    			WHEN bor.item_id = e.equipment_id then "Equipment"
+    			WHEN bor.item_id = r.room_id then "Room"
+    		END as item_type
         FROM instructor i
-        LEFT JOIN borrow bor on i.instructor_id == bor.borrower_id
-        LEFT JOIN (
-            SELECT 
-                book_id id 
-            FROM book
-            UNION
-            SELECT 
-                equipment_id 
-            FROM equipment
-            UNION
-            SELECT
-                room_id
-            FROM room
-        ) item on item.id = bor.item_id
+        LEFT JOIN borrow bor on i.instructor_id = bor.borrower_id
+        LEFT JOIN book bk on bk.book_id = bor.item_id
+        LEFT JOIN equipment e on e.equipment_id = bor.item_id 
+        LEFT JOIN room r on r.room_id = bor.item_id 
     ) as lib
