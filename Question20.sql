8cee7a3bbfe49192d489e80ab07fb2629b597d54
Vladifish <101858972+Vladifish@users.noreply.github.com>
2023-11-21 22:52:59 +0800

    Continued Question 20, select now accounts for amt. of borrowed books

--- Question20.sql
+++ Question20.sql
@@ -6,17 +6,25 @@ SELECT bk.book_id, count(bor.item_id) FROM book bk
 	GROUP BY book_id;
 	
 SELECT 
-	user.user_id, 
-	count(user_id) 
-	FROM (
-		SELECT 
-			i.instructor_id as user_id
-		FROM instructor i
-		UNION
-		SELECT
-			s.student_id
-		FROM student s
-	) as user
-	LEFT JOIN borrow bor on bor.borrower_id = user.user_id -- AND bor.end_date > "2023-01-01"
-	WHERE user.user_id = 120200201190002                   -- uncomment to see it in action
-	GROUP BY user.user_id
+		"VentoA19952018" as book_id,
+		checked_user.user_id,
+		borrow_count
+		FROM (
+			SELECT
+			user.user_id,
+			count(user_id) as borrow_count
+				FROM (
+				SELECT 
+					i.instructor_id as user_id
+				FROM instructor i
+				UNION
+				SELECT
+					s.student_id
+				FROM student s
+			) as user
+			  LEFT JOIN borrow bor on bor.borrower_id = user.user_id 
+			  LEFT JOIN book bk on bk.book_id = bor.item_id 
+			  WHERE user.user_id = 120200201190002 and bk.book_id = bor.item_id 
+			  GROUP BY user.user_id
+		) AS checked_user
+		WHERE checked_user.borrow_count < 3
